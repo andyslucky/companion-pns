77a807294fa751cc8219ff23a3ff38bf9a8317c8
Andrew Strickland <apstrick@uncg.edu>
2024-06-08 22:03:36 -0400

    Fixed docker files and bound address

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM rust:1.67
-COPY ./ ./
+FROM rust:1.78
+COPY ./ /app
+WORKDIR /app
 RUN cargo build --release
 CMD ["./target/release/abacus-pns"]
--- src/main.rs
+++ src/main.rs
@@ -218,7 +218,7 @@ async fn main() -> std::io::Result<()> {
             )
             .wrap(actix_web::middleware::Logger::new("%a %r %s %{User-Agent}i"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
